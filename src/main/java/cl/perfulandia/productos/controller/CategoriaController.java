e3afa47123316abfedfd6016793afc5a874f4abc
pauzunigaa <paulinazuniga.a@gmail.com>
2025-05-21 22:13:48 -0400

    Añadi Listar Categoria

--- src/main/java/cl/perfulandia/productos/controller/CategoriaController.java
+++ src/main/java/cl/perfulandia/productos/controller/CategoriaController.java
@@ -1,6 +1,9 @@
 package cl.perfulandia.productos.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,4 +22,9 @@ private CategoriaService categoriaService;
 public Categoria guardarCategoria(@RequestBody Categoria categoria){
     return categoriaService.guardarCategoria(categoria);
 }
+@GetMapping
+public List <Categoria>listar(){
+    return categoriaService.listarTodos();
+}
+
 }
--- src/main/java/cl/perfulandia/productos/service/CategoriaService.java
+++ src/main/java/cl/perfulandia/productos/service/CategoriaService.java
@@ -1,9 +1,12 @@
 package cl.perfulandia.productos.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cl.perfulandia.productos.model.Categoria;
+import cl.perfulandia.productos.model.Producto;
 import cl.perfulandia.productos.repository.CategoriaRepository;
 
 @Service
@@ -15,5 +18,10 @@ private CategoriaRepository categoriaRepository;
 public Categoria guardarCategoria(Categoria categoria){
     return categoriaRepository.save(categoria);}
 
+
+    // Método Listar todos los productos
+public List<Categoria> listarTodos(){
+    return categoriaRepository.findAll();
+}
 }
 
